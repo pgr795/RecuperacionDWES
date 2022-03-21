2f55bfdfbb6efd5f1bc4043680c90cafba8430ff
Pablo Garcia <90787930+pgr795@users.noreply.github.com>
2022-03-21 17:12:24 +0100

    Add files via upload

---
+++ WebJuegosAzar/Aplicacion_Hacienda/controllers/AltaSorteo_controllers.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['id']) && !isset($_SESSION['usuario'])){
+		unset($_SESSION['id']);
+		unset($_SESSION['usuario']);
+		session_destroy();
+		header("Location:../index.php");
+}
+
+include_once '../db/db.php';
+include_once '../models/AltaSorteo_model.php';
+include_once '../views/AltaSorteo_views.php';
+
+var_dump($_SESSION);
+
+$conexion=conexion();
+
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+		var_dump($_POST);
+		$dni=$_SESSION['id'];
+		$fecha=$_POST["fecha"];
+		$accion=$_POST["accion"];
+		if($accion=="Alta de Sorteo"){
+			if(isset($fecha)){
+				insertSorteo($conexion,$fecha,$dni);
+			}
+		}
+		else if($accion=="Atras"){
+			header("Location:Inicio_Hacienda_controllers.php");
+		}
+	}
+?>
